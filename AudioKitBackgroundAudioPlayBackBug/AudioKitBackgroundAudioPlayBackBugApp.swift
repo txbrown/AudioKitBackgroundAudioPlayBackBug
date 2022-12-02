85c2d9ad3d66a8da183f2b713a3ee8a14c6d4817
Ricardo <ricardo@yonkolevel.com>
2022-12-02 17:33:37 +0000

    Initial Commit

---
+++ AudioKitBackgroundAudioPlayBackBug/AudioKitBackgroundAudioPlayBackBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioKitBackgroundAudioPlayBackBugApp.swift
+//  AudioKitBackgroundAudioPlayBackBug
+//
+//  Created by Ricardo Abreu on 02/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct AudioKitBackgroundAudioPlayBackBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
